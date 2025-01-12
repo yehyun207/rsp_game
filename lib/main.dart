7b5db7d636f648a83f2eff7d54b3beb51806e099
YeHyun Byeon <oopptteh@gmail.com>
2025-01-12 20:57:09 +0900

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 class RSPApp extends StatelessWidget {
   const RSPApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
